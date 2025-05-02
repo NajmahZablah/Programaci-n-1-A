66c1d93e520ced2def02142c39ce3b22469ee052
NajmahZablah <najmahzablah@gmail.com>
2025-05-02 07:21:07 -0600

    Anexo

--- src/EstructuraCondicional.java
+++ src/EstructuraCondicional.java
@@ -3,7 +3,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
+
 
 /**
  *
@@ -56,11 +58,14 @@ public class EstructuraCondicional {
             // Ex. Factura
             
             Scanner lea=new Scanner(System.in);
-            lea.useDelimeter("\n");
+            lea.useDelimiter("\n");
+            double descuento=0, double porcentajeDsct=0, boolean clienteTerceraEdad= false;
             
             System.out.println("****Facturación - CP****");
             System.out.println("Cliente: ");
             String nombreCliente=lea.next();
+            System.out.println("Edad: ");
+            int edad=lea.nextInt();
             System.out.println("Nombre del producto: ");
             String nombre_Producto=lea.next();
             System.out.println("Cantidad del producto: ");
@@ -68,8 +73,46 @@ public class EstructuraCondicional {
             System.out.println("Precio: L.");
             double precio=lea.nextDouble();
             
-            double Pago_total=(cantidad*precio);
-            System.out.println("El total a pagar es: L.");
-           
+            
+            double subtotal=(cantidad*precio);
+       
+            if (edad>=60) {
+                clienteTerceraEdad= true;
+                descuento = (subtotal*0.3);
+                porcentajeDsct = 30;
+            } else {
+          
+                if (subtotal >= 1000 && <= 5000) {
+                    descuento = (subtotal*0.15);
+                    porcentajeDsct = 15;
+                
+                } else if (subtotal > 5000 && subtotal <= 10000 ) {
+                    if (subtotal > 10000) {
+                        descuento=(subtotal*0.35);
+                        porcentajeDsct=35;
+                    }
+                } else {
+                    descuento=(subtotal*0.25);
+                    porcentajeDsct=25;
+                }
+            }
+            
+            DecimalFormat df=new DecimalFormat("#.##");
+            double impuesto=(subtotal*0.15);
+            double total=((subtotal+impuesto)-descuento);
+                    
+            System.out.println("****Detalle de Factura****");
+            System.out.println("Subtotal: L."+ df.format(subtotal));
+            
+            if (clienteTerceraEdad) {
+            System.out.println("Observación: Cliente es de la tercera edad");
+            } else {
+            System.out.println("Observación: Cliente NO es de la tercera edad");
+            }
+            System.out.println("Descuento "+porcentajeDsct+"%: L."+ df.format(descuento));
+            System.out.println("Impuesto: L."+ df.format(impuesto));
+            System.out.println("Total: L."+ df.format(total));
+            
+            lea.close();
         }
 }
