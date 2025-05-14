faad39d2e6a0f4bcc3e5f2f2d49b85fdb36eefd3
NajmahZablah <najmahzablah@gmail.com>
2025-05-13 20:54:33 -0600

    Anexo

--- src/programación/pkg/PrimerPrograma.java
+++ src/programación/pkg/PrimerPrograma.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package programación.pkg1.a;
+package Programación.pkg;
 
 /**
  *
